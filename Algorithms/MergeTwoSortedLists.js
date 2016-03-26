bb2b5e74307d6c2a5c7f58c57285396634f58601
15cm <sinkerine@gmail.com>
2016-03-26 19:41:11 +0800

    add 19,21,23

---
+++ Algorithms/MergeTwoSortedLists.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    'use strict';
+    if(l1 === null) return l2;
+    if(l2 === null) return l1;
+    var head;
+    if(l1.val <= l2.val){
+        head = l1;
+        head.next = mergeTwoLists(l1.next,l2);
+    }else{
+        head = l2;
+        head.next = mergeTwoLists(l1,l2.next);
+    }
+    return head;
+};
