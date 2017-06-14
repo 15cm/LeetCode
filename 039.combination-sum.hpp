96f448b699cdd6065bd0f47fa99b15f3215b8934
15cm <sinkerine@gmail.com>
2017-06-14 17:20:53 +0800

    [!] Rename again

---
+++ 039.combination-sum.hpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> state(candidates.size());
+        state.assign(candidates.size(), 0);
+        sort(candidates.begin(), candidates.end());
+        search(candidates, target, state, 0, ans);
+        return ans;
+    }
+
+    void search(const vector<int>& candidates, int target, vector<int> state, int cur, vector<vector<int>>& ans) {
+        if(target == 0) {
+            vector<int> tmp;
+            for(int i = 0; i < state.size(); i ++) {
+                for(int j = 0; j < state[i]; j ++) {
+                    tmp.push_back(candidates[i]);
+                }
+            }
+            ans.push_back(tmp);
+            return;
+        }
+        if(cur >= candidates.size() || target < 0) return;
+        for(int i = 0; i <= target / candidates[cur]; i ++) {
+            state[cur] = i;
+            search(candidates, target - i * candidates[cur], state, cur + 1, ans);
+        }
+    }
+};
